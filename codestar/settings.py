dd7c9d68fa19fbdd22073f9d6d72b38d51c84ab4
lukaszkukla <lukaszkukla@gmail.com>
2022-06-30 22:22:52 +0000

    Feat: build model for commets and posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022-lk.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2022-lk.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
